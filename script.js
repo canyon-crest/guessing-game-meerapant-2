ecec10cdfa79d285bd3c6b8efaacb413511865a1
meerapant <pantm2714@my.sduhsd.net>
2026-04-10 04:43:10 +0000

    finish

--- script.js
+++ script.js
@@ -2,11 +2,42 @@
 let answer = 0;
 let guessCount = 0;
 const scores = [];
+const times = [];
+let start = 0;
 
 document.getElementById("playBtn").addEventListener("click", play);
 document.getElementById("guessBtn").addEventListener("click", makeGuess);
+document.getElementById("giveUpBtn").addEventListener("click", giveUp)
+
+let input = prompt("What is your name?");
+let casedName = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+
+function displayDate() {
+    let d = new Date();
+    let day = d.getDay();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    let currentTime = d.toLocaleTimeString();
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if ((day % 10 == 1) && (day != 11)) {
+            day += "st";
+        }
+        else if (day % 10 == 2 && day != 12) {
+            day += "nd";
+        }
+        else if (day % 10 == 3 && day != 13) {
+            day += "rd";
+        }
+        else {
+            day += "th";
+        }
+    document.getElementById("date").textContent = months[month] + " " + day + ", " + year + ". Current time: " + currentTime;
+}
+displayDate();
+setInterval(displayDate, 1000);
 
 function play(){
+    start = new Date().getTime();
     let range = 0;
     let levels = document.getElementsByName("level");
     for(let i=0; i<levels.length; i++){
@@ -15,7 +46,7 @@ function play(){
         }
         levels[i].disabled = true;
     }
-    document.getElementById("msg").textContent = "Guess a number 1-" + range;
+    document.getElementById("msg").textContent = "Hi, " + casedName + ", guess a number 1-" + range;
     answer = Math.floor(Math.random()*range) +1;
     guessCount = 0;
 
@@ -27,22 +58,46 @@ function play(){
 
 function makeGuess(){
     let guess = parseInt(document.getElementById("guess").value);
+    let diff = Math.abs(guess-answer);
     if(isNaN(guess)){
-        msg.textContent = "Please enter a valid number";
+        msg.textContent = casedName + ", please enter a valid number";
         return;
     }
     guessCount++;
     if(guess == answer){
-        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        msg.textContent = "Correct! It took " + guessCount + " tries, " + casedName + "!";
         updateScore(guessCount);
         resetGame();
     }
     else if(guess < answer){
-        msg.textContent = "Too low, try again.";
+        if (diff <= 2){
+            msg.textContent = "Too low, but getting hot, " + casedName + "!"
+        }
+            else if (diff <= 5){
+            msg.textContent = "Too low, but getting warm, " + casedName + "!"
+        }
+        else {
+            msg.textContent = "Too low, and cold, " + casedName + "!"
+        }
     }
     else{
-        msg.textContent = "Too high, try again.";
+        if (diff <= 2){
+            msg.textContent = "Too high, but getting hot, " + casedName + "!"
+        }
+            else if (diff <= 5){
+            msg.textContent = "Too high, but getting warm, " + casedName + "!"
+        }
+        else {
+            msg.textContent = "Too high, and cold, " + casedName + "!"
+        }
     }
+
+    
+//   After each wrong guess, also tell the player how close they are based on Math.abs(guess - answer):
+
+// Difference ≤ 2 → message must contain "hot" (case-insensitive)
+// Difference ≤ 5 → message must contain "warm" (case-insensitive)
+// Difference > 5 → message must contain "cold" (case-insensitive)
     
 }
 
@@ -63,6 +118,18 @@ function updateScore(score){
             lb[i].textContent = scores[i];
         }
     }
+    let timeNow =  new Date().getTime();
+    let totalTime = (timeNow - start)/1000; //convert ms into sec
+    times.push(totalTime);
+    let fastTime = Math.min(...times);
+    document.getElementById("fastest").textContent = "Fastest Game: " + fastTime.toFixed(2);
+
+    sum = 0;
+    for(let j = 0; j < times.length; j++){
+        sum += times[j];
+    }
+    avgTime.textContent = "Average Time: " + (sum/times.length).toFixed(2);
+
 }
 function resetGame(){
     guess.value = "";
@@ -73,3 +140,18 @@ function resetGame(){
     m.disabled = false;
     h.disabled = false;
 }
+
+function giveUp() {
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for(let i=0; i<levels.length; i++){
+        if(levels[i].checked){
+            range = parseInt(levels[i].value);
+        }
+    }
+    updateScore(range);
+
+    msg.textContent = "";
+    resetGame();
+}
+
